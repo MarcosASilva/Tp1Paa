1784fcf0a23c7150b0502c15b2980a2ffc794f8f
Unknown <Marcosvlk15@gmail.com>
2018-09-17 12:21:09 -0300

    Adicionada função de ordenação - Merge Sort

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "tp1.h"
-
+void imprimeV(int *v,int n){
+    int i;
+    for(i=0;i<n;i++){
+        printf("%d\n",v[i]);
+    }
+}
 
 
 int main()
@@ -9,32 +14,35 @@ int main()
     __int64 freq, inicio, fim;
 
     //Inicialização do Vetor
-    for(n=10;n<100000;n=n*10){
+    for(n=10;n<100;n=n*10){
     srand( (unsigned)time(NULL) );
 
     vet = (int *) malloc(sizeof(int) * n);
     inicializaVetor(vet,n);
 
-    printf("%d\n",n);
 
-    x = rand()%(n*10);
+    printf("Tamanho da Entrada : %d\n",n);
 
-
-    QueryPerformanceFrequency((LARGE_INTEGER *) & freq);
-    QueryPerformanceCounter((LARGE_INTEGER *) & inicio);
-    j = verificaVetor(x,vet,n);
-    QueryPerformanceCounter((LARGE_INTEGER *) & fim);
-    tempo = ((double) fim - (double) inicio) / (double) freq;
-    printf("Foi encontrado(1 - sim 0-nao) %d\n",j);
-    printf("%lf\n",tempo);
+    x = rand()%(n);
+    mergeSort(vet,0,n-1);
 
     QueryPerformanceFrequency((LARGE_INTEGER *) & freq);
     QueryPerformanceCounter((LARGE_INTEGER *) & inicio);
     j = verificaVetor2(x,vet,n);
     QueryPerformanceCounter((LARGE_INTEGER *) & fim);
+    tempo = ((double) fim - (double) inicio) / (double) freq;
+    printf("Foi encontrado(1 - sim 0-nao) %d\n",j);
+    printf("Tempo funcao 1: %lf\n",tempo);
+    tempo = 0;
+    QueryPerformanceFrequency((LARGE_INTEGER *) & freq);
+    QueryPerformanceCounter((LARGE_INTEGER *) & inicio);
+    j = verificaVetor(x,vet,n);
+    QueryPerformanceCounter((LARGE_INTEGER *) & fim);
     printf("Foi encontrado(1 - sim 0-nao) %d\n",j);
     tempo = ((double) fim - (double) inicio) / (double) freq;
-    printf("%lf\n",tempo);
+    printf("Tempo funcao otimizada:%lf\n",tempo);
+
+
 
 
 
--- tp1.c
+++ tp1.c
@@ -1,15 +1,58 @@
 #include "tp1.h"
+void merge(int vetor[], int comeco, int meio, int fim) {
+    int com1 = comeco, com2 = meio+1, comAux = 0, tam = fim-comeco+1;
+    int *vetAux;
+    vetAux = (int*)malloc(tam * sizeof(int));
+
+    while(com1 <= meio && com2 <= fim){
+        if(vetor[com1] < vetor[com2]) {
+            vetAux[comAux] = vetor[com1];
+            com1++;
+        } else {
+            vetAux[comAux] = vetor[com2];
+            com2++;
+        }
+        comAux++;
+    }
+
+    while(com1 <= meio){  //Caso ainda haja elementos na primeira metade
+        vetAux[comAux] = vetor[com1];
+        comAux++;
+        com1++;
+    }
+
+    while(com2 <= fim) {   //Caso ainda haja elementos na segunda metade
+        vetAux[comAux] = vetor[com2];
+        comAux++;
+        com2++;
+    }
+
+    for(comAux = comeco; comAux <= fim; comAux++){    //Move os elementos de volta para o vetor original
+        vetor[comAux] = vetAux[comAux-comeco];
+    }
+
+    free(vetAux);
+}
+
+void mergeSort(int vetor[], int comeco, int fim){
+    if (comeco < fim) {
+        int meio = (fim+comeco)/2;
+
+        mergeSort(vetor, comeco, meio);
+        mergeSort(vetor, meio+1, fim);
+        merge(vetor, comeco, meio, fim);
+    }
+}
 int inicializaVetor(int *v,int n){
     srand( (unsigned)time(NULL) );
     int i;
     for(i=0;i<n;i++){
-        v[i] = rand()%(n*10);
+        v[i] = rand()%n;
     }
 
 }
 
-int verificaVetor(int x,int *vet,int n)
-{   printf("Numero a ser encontrado: %d\n",x);
+int verificaVetor(int x,int *vet,int n){
     int i,j;
     for(i=0; i<n; i++)
     {
@@ -19,7 +62,7 @@ int verificaVetor(int x,int *vet,int n)
             {
                 if(vet[i]+vet[j]==x)
                 {
-                    printf("Vezes Executadas %d\n",i*j);
+
                     return 1;
                 }
             }
@@ -45,7 +88,6 @@ int verificaVetor2(int x,int *vet,int n)
             }
         }
     }
-    printf("Vezes Executadas %d\n",i*j);
     return z;
 }
 
